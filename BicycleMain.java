e1fa3a947b9666ae590723feb24d7be0d7498bbb
AshleighAvellana <grandknight799@gmail.com>
2024-10-04 14:12:06 +0800

    Update BicycleMain.java

--- BicycleMain.java
+++ BicycleMain.java
@@ -1,7 +1,7 @@
 public class BicycleMain
 {
 	public static void main(String[] args) {
-		Bicycle sportsBicycle = new Bicycle1();
+		Bicycle sportsBicycle = new Bicycle();
 		
 		System.out.println(sportsBicycle.gear);
 		sportsBicycle.braking();
