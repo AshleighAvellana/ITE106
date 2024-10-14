442d622f15d91fbef40cfb2b11581983987594ae
AshleighAvellana <grandknight799@gmail.com>
2024-10-14 14:45:02 +0800

    Update TextFileBufferedReader.java

--- Text File Manipulation/TextFileBufferedReader.java
+++ Text File Manipulation/TextFileBufferedReader.java
@@ -5,7 +5,7 @@ public class TextFileBufferedReader {
 	public static void main(String[] args) throws Exception
 	{
 	
-		File file = new File("C:\\Users\\28\\Downloads\\dadada\\Avellana.txt");
+		File file = new File("Avellana.txt");
 		
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String st;
