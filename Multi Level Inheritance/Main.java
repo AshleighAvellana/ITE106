7210bff4d73accf5d5969680c0673ec5e8654480
AshleighAvellana <grandknight799@gmail.com>
2024-11-06 22:16:08 +0800

    Delete Multi Level Inheritance directory

--- Multi Level Inheritance/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) 
-   {        
-        three g = new three();     
-        g.print_geek();
-        g.print_for();
-        g.print_lastgeek();
-    }
-}
