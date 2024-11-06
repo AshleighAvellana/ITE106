ebd26ff06065db5053bf090a64d838e99eba1781
AshleighAvellana <grandknight799@gmail.com>
2024-11-06 22:18:28 +0800

    Delete Multiple Inheritance directory

--- Multiple Inheritance/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        child c = new child();
-        c.print_geek();
-        c.print_for();
-        c.print_geek();
-    }
-}
